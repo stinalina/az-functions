89c0df1a5adb066032bb6c90761514fd1e6551a5
stinalina <38920418+stinalina@users.noreply.github.com>
2026-01-07 19:15:02 +0100

    feat: add CrawlDatabaseNightly

---
+++ CrawlDatabaseNightly.cs
@@ -0,0 +1,81 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+using System.Net.Mail;
+
+namespace Notify.Function;
+
+public class CrawlDatabaseNightly(SmtpClient smtpClient)
+{
+  private readonly string ConnectionString = Environment.GetEnvironmentVariable("DatabaseConnectionString")
+    ?? throw new InvalidOperationException("Database connection string is not set in environment variables.");
+
+	private readonly SmtpClient _smtpClient = smtpClient
+		?? throw new ArgumentNullException(nameof(smtpClient));
+
+// Cron expression: At 23:30 every day
+	[Function(nameof(CrawlDatabaseNightly))]
+	public async Task Run(
+		[TimerTrigger("0 30 23 * * *", RunOnStartup = true)] TimerInfo timerInfo,
+		FunctionContext context)
+	{
+		var logger = context.GetLogger(nameof(CrawlDatabaseNightly));
+		logger.LogInformation("CrawlDatabaseNightly function triggered.");
+
+		var tomorrow = DateTime.UtcNow.Date.AddDays(1);
+    var notifications = new List<NotificationEntry>();
+    var notificationsWithMail = new List<NotificationEntry>();
+
+    try
+    {
+      await using var conn = new NpgsqlConnection(ConnectionString);
+      await conn.OpenAsync();
+
+    var cmd = new NpgsqlCommand(
+      @"SELECT n.""Id"", n.""CreatedAt"", n.""DueDate"", n.""Content"", n.""Subject"", u.""Mail""
+      FROM dev.""Notification"" n
+      JOIN dev.""User"" u ON n.""UserId"" = u.""Id""
+      WHERE n.""DueDate""::date < @duedate", conn);
+      cmd.Parameters.AddWithValue("duedate", tomorrow);
+
+      await using var reader = await cmd.ExecuteReaderAsync();
+      while (await reader.ReadAsync())
+      {
+        notifications.Add(new NotificationEntry
+        {
+          Id = reader.GetGuid(0),
+          CreatedAt = reader.GetDateTime(1),
+          DueDate = reader.GetDateTime(2),
+          Content = reader.GetString(3),
+          Subject = reader.GetString(4),
+          Mail = reader.GetString(5),
+        });
+      }
+    }
+    catch (Exception ex)
+    {
+      logger.LogError($"Error querying database: {ex.Message}");
+    }
+
+    if (notifications.Count > 0)
+    {
+      logger.LogInformation($"Found {notifications.Count} notifications due tomorrow:");
+      foreach (var notification in notifications)
+      {
+        _smtpClient.Send("notify@remember-me.de", notification.Mail, notification.Subject, notification.Content);
+      }
+    }
+    return;
+	}
+
+  public class NotificationEntry
+  {
+    public Guid Id { get; set; }
+    public DateTime DueDate { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public required string Content { get; set; }
+    public required string Subject {get; set; }
+    public required string Mail {get; set; }
+  }
+
+}
